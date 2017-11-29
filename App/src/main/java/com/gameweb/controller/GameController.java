56e74edb3d883a3f6d3e29366e4212b12d7ef457
ciupa.kamil <194928@edu.p.lodz.pl>
2017-11-29 18:40:47 +0100

    Polaczono z baza danych, test z insert/delete

--- App/src/main/java/com/gameweb/controller/GameController.java
+++ App/src/main/java/com/gameweb/controller/GameController.java
@@ -1,5 +1,7 @@
 package com.gameweb.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,12 +13,14 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class GameController {
 
+
     @RequestMapping(value = "/test", method = RequestMethod.GET)
         public ModelAndView test(ModelAndView modelAndView){
         modelAndView.addObject("p", "TEST");
         modelAndView.setViewName("/test");
         return  modelAndView;
     }
+
 
 
 }

be5a09be10f4ab8e16be024d787c7004dc1af491
ciupa.kamil <194928@edu.p.lodz.pl>
2017-12-14 19:10:07 +0100

    Dodawanie zdjęcia z front do back

--- App/src/main/java/com/gameweb/controller/UserController.java
+++ App/src/main/java/com/gameweb/controller/UserController.java
@@ -7,6 +7,7 @@ import com.gameweb.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
@@ -15,6 +16,9 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Base64;
 
 /**
@@ -52,5 +56,32 @@ public class UserController {
         modelAndView.setViewName("/registrationTest");
         return  modelAndView;
     }
+
+
+    @RequestMapping(value = "/uploadFile", method = RequestMethod.GET)
+    public ModelAndView uploadGet(){
+        modelAndView.setViewName("/uploadPictureTest");
+        return  modelAndView;
+    }
+
+    @RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
+    public ModelAndView uploadPost(@RequestParam("file") MultipartFile uploadfile){
+
+        modelAndView.addObject("file" , uploadfile);
+        String s = "";
+        try {
+            byte[] bytes = uploadfile.getBytes();
+            s = new sun.misc.BASE64Encoder().encode(bytes);
+            System.out.println(s);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        modelAndView.addObject("img", "data:image/png;base64,"+ s);
+        modelAndView.setViewName("/uploadPictureTest");
+        return  modelAndView;
+    }
+
 
 }

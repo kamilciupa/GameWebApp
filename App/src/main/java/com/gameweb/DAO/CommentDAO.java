2e3c9668dad3fdae050678e0cbedee67fb84b0c8
Kamil Ciupa <kamilciupa23@gmail.com>
2018-04-24 21:50:14 +0200

    Stage 1 reformat

--- App/src/main/java/com/gameweb/DAO/CommentDAO.java
+++ App/src/main/java/com/gameweb/DAO/CommentDAO.java
@@ -1,5 +1,6 @@
 package com.gameweb.DAO;
 
+import ch.qos.logback.core.LogbackException;
 import com.gameweb.model.Comment;
 import com.gameweb.service.UserService;
 import com.gameweb.utils.Queries;
@@ -10,82 +11,99 @@ import org.springframework.stereotype.Repository;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.MessageFormat;
 import java.util.List;
 
 @Repository
 public class CommentDAO {
 
+  @Autowired JdbcTemplate jdbcTemplate;
+  @Autowired UserService userService;
 
-        @Autowired
-        JdbcTemplate jdbcTemplate;
-        @Autowired
-    UserService userService;
+  Queries queries = new Queries();
+  String className = "CommentDAO";
 
-        // Make Bean and autowire that
-        Queries queries = new Queries();
-
-
-        public void addComment(Comment comment){
-            try{
-                jdbcTemplate.update(queries.I_COMMENT,comment.getContent(),comment.getParent(),comment.getType(),comment.getKey_value(),comment.getAuthor());
-            } catch (Exception e){
-                e.printStackTrace();
-                System.out.println("BLAD");
-            }
-        }
-
-        public List<Comment> getCommentsForGame(String gameTitle){
-                List<Comment> a = jdbcTemplate.query(queries.S_GET_COMMS, new RowMapper<Comment>() {
-                    @Override
-                    public Comment mapRow(ResultSet resultSet, int i) throws SQLException {
-                        Comment c = new Comment();
-                        c = setCommentFromQuery(resultSet, c);
-                        return c;
-                    }
-                },gameTitle);
-            return a;
-        }
-
-        public List<Comment> getChildComment(int parent){
-            List<Comment> a = jdbcTemplate.query(queries.S_GET_CHILD_COMMS, new RowMapper<Comment>() {
-                @Override
-                public Comment mapRow(ResultSet resultSet, int i) throws SQLException {
-                    Comment c = new Comment();
-                    c = setCommentFromQuery(resultSet,c);
-                    return c;
-                }
-            },parent);
-             return a;
-        }
-
-    private Comment setCommentFromQuery(ResultSet resultSet, Comment c) throws SQLException {
-        c.setContent(resultSet.getString("content"));
-        c.setType(resultSet.getString("type"));
-        c.setKey_value(resultSet.getInt("key_value"));
-        c.setAuthor(resultSet.getInt("author"));
-        c.setId(resultSet.getInt("id"));
-        c.setParent(resultSet.getInt("parent"));
-        c.setAuthor_name(resultSet.getString("username"));
-        c.setAuthor_avatar("data:image/png;base64,"+getAvatarString(resultSet.getBytes("avatar")));
-        return c;
+    private void printMyException(String className, String methodName, Exception e) {
+        System.out.println(
+                MessageFormat.format(
+                        "1) {0}\n2) {1}\n3) {2}\n4) {3}",
+                        className, methodName, e.getMessage(), e.getClass().getCanonicalName()));
     }
 
 
-    private String getAvatarString(byte[] avatar) {
-        String s = "";
-        byte[] bytes;
-        try {
-            if (avatar == null) {
-                bytes = userService.getDefaultAvatar();
-            } else {
-                bytes = avatar;
-            }
-            org.apache.commons.codec.binary.Base64 encoder = new org.apache.commons.codec.binary.Base64();
-            s = encoder.encodeToString(bytes);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return s;
+  public void addComment(Comment comment) {
+    try {
+      jdbcTemplate.update(
+          queries.I_COMMENT,
+          comment.getContent(),
+          comment.getParent(),
+          comment.getType(),
+          comment.getKey_value(),
+          comment.getAuthor());
+    } catch (Exception e) {
+      printMyException(className, "addComment", e);
     }
+  }
 
+
+  public List<Comment> getCommentsForGame(String gameTitle) {
+    List<Comment> a =
+        jdbcTemplate.query(
+            queries.S_GET_COMMS,
+            new RowMapper<Comment>() {
+              @Override
+              public Comment mapRow(ResultSet resultSet, int i) throws SQLException {
+                Comment c = new Comment();
+                c = setCommentFromQuery(resultSet, c);
+                return c;
+              }
+            },
+            gameTitle);
+    return a;
+  }
+
+  public List<Comment> getChildComment(int parent) {
+    List<Comment> a =
+        jdbcTemplate.query(
+            queries.S_GET_CHILD_COMMS,
+            new RowMapper<Comment>() {
+              @Override
+              public Comment mapRow(ResultSet resultSet, int i) throws SQLException {
+                Comment c = new Comment();
+                c = setCommentFromQuery(resultSet, c);
+                return c;
+              }
+            },
+            parent);
+    return a;
+  }
+
+  private Comment setCommentFromQuery(ResultSet resultSet, Comment c) throws SQLException {
+    c.setContent(resultSet.getString("content"));
+    c.setType(resultSet.getString("type"));
+    c.setKey_value(resultSet.getInt("key_value"));
+    c.setAuthor(resultSet.getInt("author"));
+    c.setId(resultSet.getInt("id"));
+    c.setParent(resultSet.getInt("parent"));
+    c.setAuthor_name(resultSet.getString("username"));
+    c.setAuthor_avatar("data:image/png;base64," + getAvatarString(resultSet.getBytes("avatar")));
+    return c;
+  }
+
+  private String getAvatarString(byte[] avatar) {
+    String s = "";
+    byte[] bytes;
+    try {
+      if (avatar == null) {
+        bytes = userService.getDefaultAvatar();
+      } else {
+        bytes = avatar;
+      }
+      org.apache.commons.codec.binary.Base64 encoder = new org.apache.commons.codec.binary.Base64();
+      s = encoder.encodeToString(bytes);
+    } catch (Exception e) {
+        printMyException(className, "getAvatarString", e);
+    }
+    return s;
+  }
 }
